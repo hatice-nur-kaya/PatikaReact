5d0589639d5784aed9944ca18e31cd863e5712a1
drhnk2607 <haticenur.4455@gmail.com>
2022-08-21 00:33:36 +0300

    Patika
    Patika

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { getData } from './getData'
+
+function App() {
+    getData(1).then((res) => console.log(res.getUser.data, res.getPost.data));
+
+    return <div className="App"></div>;
+}
+
+export default App;
